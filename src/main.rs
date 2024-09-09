e548b62b6e401b882b44cc4534f4276e79df7cb8
1 <nakumogana@gmail.com>
2024-09-09 15:31:24 +1000

    Debug

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-
 fn main()
 {
     let int: i32 = 6969;
@@ -6,5 +5,23 @@ fn main()
     let real: f64 = 3.1415926;
 
     println!("{real:.3}");
+
+
+    #[derive(Debug)]
+    struct Wakka(i32);
+
+    let a:Wakka = Wakka(42);
+
+    #[derive(Debug)]
+    struct WakkaWakka(Wakka);
+
+    let b:WakkaWakka = WakkaWakka(Wakka(69));
+
+
+    println!("{a:?}\n");
+    println!("{a:#?}");
+
+    println!("{b:?}\n");
+    println!("{b:#?}");
 
 }
